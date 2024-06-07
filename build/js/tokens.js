5831252cde5558676d70f0078d849d7e37e61daf
oLemos <lemmos@outlook.com.br>
2024-06-07 18:21:15 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 07 Jun 2024 21:18:44 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorPurple500": "#8b5cf6",
+  "ColorGray500": "#71717a",
+  "ColorGray800": "#18181b",
+  "ColorGray900": "#09090b"
+};
